fe81326076fd6602150f0965d3a23a2f545a22e4
DPatter52 <dzpatterson15@gmail.com>
2022-05-15 02:49:17 -0600

    Massive update for Chamber home page

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,4 +1,16 @@
+const datefield = document.querySelector(".headerDate");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
 const lastModified = new Date (document.lastModified);
 document.querySelector("#currentdate").textContent = lastModified;
 
-//https://codepen.io/blazzard-jason/pen/ZEWjeaw//
+
+
+const hambutton = document.querySelector('.hamburger');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
