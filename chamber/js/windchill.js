0ebe1ca3890225a6eca28dcc2c1e57b93fde8711
DPatter52 <dzpatterson15@gmail.com>
2022-06-03 22:28:12 -0600

    week 6 updates

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -8,7 +8,6 @@ const chillFactor = document.querySelector('.windchill')
 
 const windChillFahrenheit = (temperature, windSpeed) =>{
     
-   
     if (temperature <= 50 && windSpeed >= 3 ){
         windChillFactor = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 
         0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
@@ -21,7 +20,6 @@ const windChillFahrenheit = (temperature, windSpeed) =>{
         windChillFactor = 'N/A';
         console.log(windChillFactor)
         chillFactor.innerHTML = windChillFactor;
-
 }
 
 }
