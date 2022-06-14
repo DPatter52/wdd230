62718532394636e97a68a7e66d6b35d904f86d75
DPatter52 <dzpatterson15@gmail.com>
2022-06-14 16:18:06 -0600

    Join page and thank you page

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -8,7 +8,7 @@ const lastModified = new Date (document.lastModified);
 document.querySelector("#currentdate").textContent = lastModified;
 
 
-
+//----Hamburger Nav----//
 const hambutton = document.querySelector('.hamburger');
 const mainnav = document.querySelector('.navigation')
 
@@ -16,3 +16,11 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 
+//----Join Page Form Submission----//
+const subdate = document.querySelector("#submissiondate");
+
+const dateOfSub = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+subdate.innerHTML = `<em>${fulldate}</em>`; 
+
+console.log(dateOfSub)
