013e3a4afbae9ea720660de953746eede67aacc8
DPatter52 <dzpatterson15@gmail.com>
2022-06-24 10:10:14 -0600

    upsate to weatherAPI

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,39 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// create url that stores api 
+const url = `{https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=27815bec9f6207729f966614a7207170}`;
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  }
+  
+  apiFetch();
+
+
+
+
+
+
+
+
+
+
