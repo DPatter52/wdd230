dbc0bc6682331e87eed7adac56715efbd5dc213c
DPatter52 <dzpatterson15@gmail.com>
2022-06-25 07:53:44 -0600

    Finished weatherAPI practice

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -4,40 +4,36 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 // create url that stores api 
-const url = `{https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=27815bec9f6207729f966614a7207170}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=27815bec9f6207729f966614a7207170`;
 
 
 async function apiFetch() {
-    try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
+        
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
-    } catch (error) {
-        console.log(error);
     }
-  }
+  
 
-  function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  }
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
 
-  function  displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+  const upperDesc = desc.toUpperCase()
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = upperDesc;
+
+}
+
   
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const desc = weatherData.weather[0].description;
-  
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-  }
-  
-  apiFetch();
+apiFetch();
 
 
 
