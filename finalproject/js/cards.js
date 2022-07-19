9178b6bf3667363ddbd0d11c3f3c464c737f99b3
DPatter52 <dzpatterson15@gmail.com>
2022-07-19 10:55:42 -0600

    json update

--- finalproject/js/cards.js
+++ finalproject/js/cards.js
@@ -28,9 +28,9 @@ function displayTemples(data) {
         
         p1.innerHTML = `${temple.templename}`;
         p2.innerHTML = `${temple.telephone}`;
-        //p3.innerHTML = `${temple.services}`;
-        //p4.innerHTML = `${temple.history}`;
-        //p5.innerHTML = `${temple.templeclosure}`;
+        p3.innerHTML = `${temple.services}`;
+        p4.innerHTML = `${temple.history}`;
+        p5.innerHTML = `${temple.templeclosure}`;
         img.setAttribute('src', temple.templeicon);
         img.setAttribute('alt', `Icon for ${temple.templename}`);
         img.setAttribute('loading', 'lazy');
@@ -38,9 +38,9 @@ function displayTemples(data) {
         card.append(img);
         card.append(p1);
         card.append(p2);
-        //card.append(p3);
-        //card.append(p4);
-       // card.append(p5);
+        card.append(p3);
+        card.append(p4);
+        card.append(p5);
         
         cards.appendChild(card);
     });
